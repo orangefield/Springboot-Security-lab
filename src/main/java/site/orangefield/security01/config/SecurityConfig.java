8e59cb71eb44563ed4deb42046564196e7972173
field <jsw_777@naver.com>
2022-06-10 17:41:09 +0900

    Security 주소 환경 설정

---
+++ src/main/java/site/orangefield/security01/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package site.orangefield.security01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity // Security 활성화, 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 된다
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/user/**").authenticated() // 인증이 필요
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") // 권한이 있어야
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll() // 위의 주소가 아니라면 모든 권한이 있음
+                .and()
+                .formLogin()
+                .loginPage("/login");
+
+    }
+}
--- src/main/java/site/orangefield/security01/controller/IndexController.java
+++ src/main/java/site/orangefield/security01/controller/IndexController.java
@@ -2,6 +2,7 @@ package site.orangefield.security01.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // View를 리턴하겠다
 public class IndexController {
@@ -12,5 +13,36 @@ public class IndexController {
         // mustache 기본 폴더 src/main/resources/
         // view resolver 설정 templates(prefix), .mustache(suffix) - 의존성 설정했기 때문에 생략 가능
         return "index"; // src/main/resources/templates/index.mustache를 찾을 것
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager() {
+        return "manager";
+    }
+
+    // Security가 이 주소를 낚아챈다, SecurityConfig 파일 생성 후 낚아채지 않는다
+    @GetMapping("/login")
+    public @ResponseBody String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc() {
+        return "회원가입 완료됨";
     }
 }
